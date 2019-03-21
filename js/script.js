87a4703d1f6bd32a521b826634e410af42d37848
Elvis255 <emaliza2482@gmail.com>
2019-03-21 22:20:46 +0000

    Day 1 & 2

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var userInput=$("#search-term").val();
+   var url = "https://api.giphy.com/v1/gifs/search?q="+userInput+"&rating=pg&api_key=dc6zaTOxFJmzC"
+   $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response){
+            if (response.data.length > 0) {
+                $('body').append("<img src= "+response.data[0].images.original_still.url+ ">" );
+            }
+        },
+    });
+
+
 });
 
